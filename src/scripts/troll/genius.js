e61ceb0e620554269b0e27aff7e4820c04c876e0
HoanBuCon <136064038+HoanBuCon@users.noreply.github.com>
2026-08-28 15:24:27 +0700

    chore(project-root): i changed something

--- src/scripts/troll/genius.js
+++
@@ -1,7 +0,0 @@
-console.log("Genius check: 1 + 1 === 3?");
-
-setTimeout(() => {
-    const genius = () => 1 + 1 === 3;
-    console.log("Genius check:", !genius());
-    console.log("System started!");
-}, 3000);
